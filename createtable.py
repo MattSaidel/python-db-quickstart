5eb3a8233b17578b67a83c4755d90d983cdc4c70
MattSaidel <masaidel@gmail.com>
2013-12-09 21:24:59 -0500

    stuck

--- createtable.py
+++ createtable.py
@@ -1,29 +1,25 @@
 from datasource import dbconfig, engines
 from sqlalchemy import Column, Integer, String, MetaData, Table
+import csv
 
-
-# TODO get a connection and do stuff in this main method
-# You absolutely must read these docs:
-# http://docs.sqlalchemy.org/en/rel_0_9/core/tutorial.html
 def main():
 
     db_engine = engines.get_postgres_engine()
 
     metadata = MetaData()
-    people = Table('users', metadata,
+    people = Table('people', metadata,
                   Column('id', Integer, primary_key=True),
-                  Column('name', String),
-                  Column('fullname', String),
+                  Column('first_name', String),
+                  Column('last_name', String),
+                  Column('age', Integer)
                   )
     metadata.create_all(db_engine)
+    
+    data = csv.DictReader(open("test.csv"))
 
-    print 'line one'
-    print 'another line'
-    print 'hello world'
-    print 'go check the postgres database'
+    for row in data:
 
-
-
+    db_engine.execute(people.insert(), [data])
 
 if __name__ == '__main__':
     # execute this block of code if this script is run from command line
